cbe87f4aad9a1d1748bc84b09059737f4f1de2e7
Hassan <hassanalihazaraa@gmail.com>
2020-08-11 12:32:51 +0200

    added student form

--- Controller/Student.controller.php
+++ Controller/Student.controller.php
@@ -5,6 +5,20 @@ class StudentController
 {
     public function render()
     {
+        if (empty($_POST['class'])) {
+            $classLoader = new ClassLoader();
+            $classes = $classLoader->getClass();
+        }
+        if ($_POST['submit'] === 'submit') {
+            $fullName = htmlspecialchars(trim($_POST['name']));
+            $email = htmlspecialchars(trim($_POST['email']));
+            $classId = (int)$_POST['class'];
+            $students = new Student($fullName, $email, $classId);
+            if ($classId !== 0) {
+                $students->setClassId($classId);
+            }
+            $students->saveStudent(DatabaseConnection::connect());
+        }
         require_once 'View/student.view.php';
     }
 }
--- View/student.view.php
+++ View/student.view.php
@@ -3,24 +3,56 @@ declare(strict_types=1);
 
 require_once 'View/includes/Header.php';
 ?>
-    <form>
-        <div class="form-row">
-            <div class="col-md-6 mb-3">
-                <label for="validationServer01">First name</label>
-                <input type="text" class="form-control is-valid" id="validationServer01" value="Mark" required>
-                <div class="valid-feedback">
-                    Looks good!
+
+<?php if (isset($_POST['submit'])) {
+    echo '<div class="alert alert-success" role="alert">
+              Your form is submitted <a href="?page=student" class="alert-link"Back to Student</a>
+          </div>';
+} else {
+    echo '<div class="alert alert-danger" role="alert">
+             Please the form and try again!
+          </div>';
+}
+?>
+    <section class="container-fluid">
+        <h2 class="text-center mb-5">Create a new student</h2>
+        <form method="post">
+            <section class="container-fluid">
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                        <label for="validationServer01">Full Name</label>
+                        <input type="text" class="form-control" name="name" id="validationServer01" value="" required>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="validationServer02">Email</label>
+                        <input type="text" class="form-control" name="email" id="validationServer02" value="" required>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="validationServer04">Class</label>
+                        <select class="custom-select class" name="class" id="validationServer04"
+                                aria-describedby="validationServer04Feedback" required>
+                            <option selected disabled value="">Choose a class</option>
+                            <?php
+                            /** @var ClassModel[] $classes */
+                            foreach ($classes as $class) {
+                                echo "<option value='{$class->getId()}'>{$class->getName()}</option>";
+                            }
+                            ?>
+                        </select>
+                        <div id="validationServer04Feedback" class="invalid-feedback">
+                            Please select a class.
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-6 mb-3">
-                <label for="validationServer02">Last name</label>
-                <input type="text" class="form-control is-valid" id="validationServer02" value="Otto" required>
-                <div class="valid-feedback">
-                    Looks good!
-                </div>
-            </div>
-        </div>
-        <button class="btn btn-primary" type="submit">Submit form</button>
-    </form>
+                <button class="btn btn-primary w-100" type="submit" value="submit">Create a new student</button>
+            </section>
+        </form>
+    </section>
 
 <?php require_once 'View/includes/Footer.php'; ?>
